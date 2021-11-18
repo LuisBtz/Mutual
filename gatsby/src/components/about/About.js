92e6981ba0f7eb2c01bf43eef5dc09170a674c76
Luis B <luisbttf@gmail.com>
2021-11-17 19:41:59 -0600

    Contacto

--- gatsby/src/components/about/About.js
+++ gatsby/src/components/about/About.js
@@ -7,7 +7,7 @@ const About = ({data}) => {
 
     return(
         <AboutContainer>
-            <AboutSection>
+            <AboutSection className='item'>
                 <div className='left'>
                     <h1>(ABOUT US)</h1>
                 </div>
@@ -17,30 +17,75 @@ const About = ({data}) => {
                     />
                 </div>
             </AboutSection>
-            <PressSection>
+            <PressSection className='item'>
                 <div className='left'>
                         <h2>(Press)</h2>
-                    </div>
-                <div className='right'>
-                {data.sanityAboutPage.press.map(({ title, url, _key }) => {
-                    return (
-                        <li key={_key}><a target='_blank' rel="noreferrer" href={url}>{title}</a></li>
-                    )
-                })}
                 </div>
+                <ul className='right'>
+                    {data.sanityAboutPage.press.map(({ title, url, _key }) => {
+                        return (
+                            <li key={_key}><a target='_blank' rel="noreferrer" href={url}>{title}</a></li>
+                        )
+                    })}
+                </ul>
             </PressSection>
         </AboutContainer>
     )
 }
 
 const AboutContainer = styled.section`
-
+padding-top: 100px;
+width: 100%;
+margin: 0 auto;
+display: grid;
+grid-template-columns: repeat(12, 1fr);
+.item {
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 650px) {
+        padding: 20px;
+    }
+    h1, h2 {
+        font-size: 1rem;
+        font-style: normal;
+        font-weight: normal;
+        position: static;
+        width: 150px;
+    }
+    ul {
+        list-style: none;
+        li {
+            a {
+                text-transform: uppercase;
+                color: black;
+            }
+        }
+    }
+    p {
+        text-transform: uppercase;
+    }
+}
 `
 
 const AboutSection = styled.div`
+grid-column: 6/10;
+grid-row: 1;
+@media (max-width: 950px) {
+    grid-column: 5/11;
+}
+@media (max-width: 650px) {
+    grid-column: 1/13;
+}
 
 `
 const PressSection = styled.div`
-
+grid-row: 2;
+grid-column: 6/10;
+@media (max-width: 950px) {
+    grid-column: 5/11;
+}
+@media (max-width: 650px) {
+    grid-column: 1/13;
+}
 `
 export default About 
